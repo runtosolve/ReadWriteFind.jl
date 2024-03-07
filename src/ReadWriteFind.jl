bbd093ffa0d4ae98571316ca217e60445a5b357b
Cris Moen <cris.moen@runtosolve.com>
2024-03-07 09:48:04 -0500

    add string chunk finders

--- src/ReadWriteFind.jl
+++ src/ReadWriteFind.jl
@@ -41,4 +41,37 @@ function write_file(save_filename, lines)
 end
 
 
+function find_phrase_in_string_chunk(string_chunk, search_name)
+
+    chunk_numbers = Vector{Int}(undef, 0)
+    for i in eachindex(string_chunk)
+
+        if contains(string_chunk[i], search_name)
+            push!(chunk_numbers, i)
+        end
+
+    end
+
+    return chunk_numbers
+
+end
+
+
+function get_specific_string_chunk(string_chunk, search_name_start, start_offset, search_name_end, end_offset)
+
+    chunk_numbers = ReadWriteFind.find_phrase_in_string_chunk(string_chunk, search_name_start)
+    start_data_index = chunk_numbers[1] + start_offset
+
+    string_chunk = string_chunk[start_data_index:end]
+
+    chunk_numbers = ReadWriteFind.find_phrase_in_string_chunk(string_chunk, search_name_end)
+    end_data_index = chunk_numbers[1] + end_offset
+
+    chunk = string_chunk[1:end_data_index]
+
+    return chunk
+
+end
+
+
 end # module ReadWriteFind
